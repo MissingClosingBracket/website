493370702ea9bc027a629702d160b5606d0b76a2
Christian Hedegaard Pliess Larsen - Implement Consulting Group <chla@implement.dk>
2021-03-13 16:25:53 +0100

    documentation for api. Now needs to be implemented

--- news.js
+++ news.js
@@ -1,10 +1,62 @@
 
 
-function getNews() {
-    var apiKey = "5b8eb55b32374c289ceb8faf14f34f89"
-    var keyWord = "tesla"
+async function getNews() {
 
-    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=5b8eb55b32374c289ceb8faf14f34f89').then(data => console.log(data));
-   
+    var requestOptions = {
+        method: 'GET'
+      };
+      
+    fetch("http://api.mediastack.com/v1/news?access_key=4b433178658deb64a15eadfb0c390354&keywords=corona", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(JSON.parse(result)))
+    .catch(error => alert(error));
+    
+
 }
 
+
+/**
+ * Categories: "&categories=business,sports,-science"
+        general - Uncategorized News
+        business - Business News
+        entertainment - Entertainment News
+        health - Health News
+        science - Science News
+        sports - Sports News
+        technology - Technology News
+
+ * Sources: "&sources=cnn,-bbc"
+        no listing available.. will make own? TODO
+
+ * Countires: "&countries=ar,br,-ro"
+        list: ar,au,at,be,br,bg,ca,cn,co,cz,eg,fr,de,gr,hk,hu,in,id,ie,il,it,jp,lv,lt,my,mx,ma,nl,
+              nz,ng,no,ph,pl,pt,ro,sa,rs,sg,sk,si,za,kr,se,ch,tw,th,tr,ae,ua,gb,us,ve
+ 
+ * Languages: "&languages=ar,en,-no"
+        ar - Arabic
+        de - German
+        en - English
+        es - Spanish
+        fr - French
+        he - Hebrew
+        it - Italian
+        nl - Dutch
+        no - Norwegian
+        pt - Portuguese
+        ru - Russian
+        se - Swedish
+        zh - Chinese
+ 
+ * Keywords: "&keywords=tesla" OBS: only supports one keyword atm. Maybe contact mediastack? TODO
+ 
+ * Date: "&date=2020-01-01" or "&date=2020-01-01,2020-12-31"
+ 
+ * Sort: "&sort=[option]"
+        published_desc (default), published_asc, popularity
+  
+ * Limit: "&limit=100"
+        default is 25
+ 
+ * Offset: "&offset=100"
+        deafult is 0. Why use an offset..? Might be useful.
+ **/
